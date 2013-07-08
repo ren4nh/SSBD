d4c194b6d068776313064d73fb49fabcf973dec1
ren4nh <ren4nh@70f7f2f6-02dd-41cf-9970-37a83fcd02c4>
2013-07-08 16:59:07 +0000

    Adição do equals pelo nome na classe Tabela

--- Biblioteca/src/br/com/tcc/bean/Tabela.java
+++ Biblioteca/src/br/com/tcc/bean/Tabela.java
@@ -2,13 +2,14 @@ package br.com.tcc.bean;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  *
  * @author renan
  */
 public class Tabela {
-    
+
     private String nome;
     private List<Coluna> listaColuna = new ArrayList<>();
 
@@ -32,10 +33,28 @@ public class Tabela {
     }
 
     @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Tabela other = (Tabela) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
     public String toString() {
         return "Tabela{" + "nome=" + nome + '}';
     }
-    
-    
-    
 }
